4b206117f48f7bb3e27250cf014f3bbfd8c88624
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2022-07-08 23:51:26 +0300

    seed data

--- MVCTest/Program.cs
+++ MVCTest/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MvcTest.Models;
 using MVCTest.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MoviesContext>(options =>
@@ -16,6 +17,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
 }
 
 app.UseHttpsRedirection();
