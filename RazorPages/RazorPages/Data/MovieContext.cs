8e9b3c5dd2768aef384b462659fd0a34a84c1a1f
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2022-07-09 22:20:32 +0300

    Add razor pages project

---
+++ RazorPages/RazorPages/Data/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Models;
+
+namespace RazorPages.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPages.Models.Movie>? Movie { get; set; }
+    }
+}
---
+++ RazorPages/RazorPages/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPages.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required, StringLength(maximumLength: 60, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+        
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [StringLength(maximumLength: 30), RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(minimum: 1, maximum: 100), Column(TypeName ="decimal(18,2)"), DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required, StringLength(maximumLength: 5), RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string Rating { get; set; } = string.Empty;
+    }
+}
