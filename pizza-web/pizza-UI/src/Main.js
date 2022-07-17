8f62a7de03d8feba313a0bd0b4470b0838a9706c
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2022-07-17 20:57:46 +0300

    add MicroServices

--- pizza-web/pizza-UI/src/Main.js
+++ pizza-web/pizza-UI/src/Main.js
@@ -3,50 +3,50 @@ import React, { useState, useEffect } from "react";
 const Pizza = ({ pizza }) => {
    const [data, setData] = useState(pizza);
    const [dirty, setDirty] = useState(false);
-   
+
    function update(value, fieldName, obj) {
-      setData({ ...obj, [fieldName] : value });
+      setData({ ...obj, [fieldName]: value });
       setDirty(true);
    }
-   
+
    function onSave() {
       setDirty(false);
       // make rest call
    }
-   
+
    return (<React.Fragment>
       <div>
-      <h3>
-         <input onChange={(evt) => update(evt.target.value, 'name', data)} value={data.name} /> 
-      </h3>
-      <div>
-         <input onChange={(evt) => update(evt.target.value, 'description', data)} value={data.description} />
-      </div>
-      {dirty ? 
-      <div><button onClick={onSave}>Save</button></div> : null
-      }
+         <h3>
+            <input onChange={(evt) => update(evt.target.value, 'name', data)} value={data.name} />
+         </h3>
+         <div>
+            <input onChange={(evt) => update(evt.target.value, 'description', data)} value={data.description} />
+         </div>
+         {dirty ?
+            <div><button onClick={onSave}>Save</button></div> : null
+         }
       </div>
    </React.Fragment>);
 }
 
 const Main = () => {
-  const [pizzas, setPizzas] = useState([]);
-  
-  useEffect(() => {
-    fetchData();
-  }, [])
+   const [pizzas, setPizzas] = useState([]);
 
-  function fetchData() {
-    fetch("/api/pizza")
-      .then(response => response.json())
-      .then(data => setPizzas(data)) 
-  }
+   useEffect(() => {
+      fetchData();
+   }, [])
 
-  const data = pizzas.map(pizza => <Pizza pizza={pizza} />)
+   function fetchData() {
+      fetch("/api/pizza")
+         .then(response => response.json())
+         .then(data => setPizzas(data))
+   }
 
-  return (<React.Fragment>
-   {data}
-  </React.Fragment>)
+   const data = pizzas.map(pizza => <Pizza pizza={pizza} />)
+
+   return (<React.Fragment>
+      {data}
+   </React.Fragment>)
 }
 
 export default Main;
