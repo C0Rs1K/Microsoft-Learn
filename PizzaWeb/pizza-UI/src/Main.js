b148207a566e22d744e461e7d0702ac234c3b0f6
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2022-07-20 01:57:13 +0300

    rename files

---
+++ PizzaWeb/pizza-UI/src/Main.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+
+const Pizza = ({ pizza }) => {
+   const [data, setData] = useState(pizza);
+   const [dirty, setDirty] = useState(false);
+
+   function update(value, fieldName, obj) {
+      setData({ ...obj, [fieldName]: value });
+      setDirty(true);
+   }
+
+   function onSave() {
+      setDirty(false);
+      // make rest call
+   }
+
+   return (<React.Fragment>
+      <div>
+         <h3>
+            <input onChange={(evt) => update(evt.target.value, 'name', data)} value={data.name} />
+         </h3>
+         <div>
+            <input onChange={(evt) => update(evt.target.value, 'description', data)} value={data.description} />
+         </div>
+         {dirty ?
+            <div><button onClick={onSave}>Save</button></div> : null
+         }
+      </div>
+   </React.Fragment>);
+}
+
+const Main = () => {
+   const [pizzas, setPizzas] = useState([]);
+
+   useEffect(() => {
+      fetchData();
+   }, [])
+
+   function fetchData() {
+      fetch("/api/pizza")
+         .then(response => response.json())
+         .then(data => setPizzas(data))
+   }
+
+   const data = pizzas.map(pizza => <Pizza pizza={pizza} />)
+
+   return (<React.Fragment>
+      {data}
+   </React.Fragment>)
+}
+
+export default Main;
---
+++ WebApi/Web-Api/Controllers/PizzaController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Api.Models;
+using Web_Api.Services;
+
+namespace Web_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+            PizzaService.GetAll();
+
+        // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+        // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
+        // PUT action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (pizza.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+            {
+                return NotFound();
+            }
+            
+            PizzaService.Update(pizza);
+            return NoContent();
+        }
+        // DELETE action
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if (pizza is null)
+            {
+                return NotFound();
+            }
+            
+            PizzaService.Delete(id);
+            
+            return NoContent();
+        }
+    }
+}
